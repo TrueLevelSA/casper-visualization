1ee1394aed8494855855f47db81dc9b3855791d8
Robert Hambrock <roberthambrock@gmail.com>
2018-11-19 15:48:21 +0100

    Add visualization files

---
+++ gif_processing/process.sh
@@ -0,0 +1,41 @@
+# #!/usr/local/bin/zsh
+cp states.dat states2.dat
+gsed -i '1d' states2.dat
+gsed -i '/tests/d' states2.dat
+gsed -i '/test example::/d' states2.dat
+gsed -i '/test result:/d' states2.dat
+# gsed -i '1d' states2.dat
+# gsed -i '1d' states2.dat
+# gsed -i '1d' states2.dat
+# gsed -i '1d' states2.dat
+# gsed -i '$d' states2.dat
+# gsed -i '$d' states2.dat
+# gsed -i '$d' states2.dat
+# gsed -i '$d' states2.dat
+# gsed -i '$d' states2.dat
+# gsed -i '$d' states2.dat
+# gsed -i '$d' states2.dat
+# gsed -i '$d' states2.dat
+# gsed -i '$d' states2.dat
+csplit states2.dat '/new chain/' '{100}'
+for file in xx*; do
+    gsed -i '1d' $file
+    echo "[\n$(cat $file)" > $file
+    # perl -ni -e 'print unless $. == 2' $file
+    gsed -i 's/Some(Block(ProtoBlock//g' $file
+    gsed -i 's/Block(ProtoBlock//g' $file
+    gsed -i 's/, txs: {} //g' $file
+    gsed -i 's/LatestMsgs(//g' $file
+    gsed -i 's/(//g' $file
+    gsed -i 's/)//g' $file
+    # gsed -i 's/]/],/g' $file
+    # gsed -i 's/a}/},/g' $file
+    # gsed -i 's/quwel\{/[/g' $file
+    # gsed -i 's/\}quwel/]/g' $file
+    gsed -i '$ s/.$//' $file
+    echo "]" >> $file
+    gsed -i 's/],\n]/]\n]/g' $file
+    gsed -i 's/M\([[:digit:]]\)/M\1:/g' $file
+    sed -E "s/([a-z]+|N[a-z]+|M[0-9]+|[0-9]+)/\"\\1\"/g" $file > processed$file'.json'
+done
+# cat states2.dat | less
